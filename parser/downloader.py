8d137816d978eac1951fc5f168d8f72ce7c2a392
musa jamshed <Musa@musas-MacBook-Air.local>
2017-10-20 08:58:45 -0400

    figured out how to parse, got times and incident type

--- parser/downloader.py
+++ parser/downloader.py
@@ -1,5 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
 
-resp = requests.get('https://police.lehigh.edu/')
 
-print(resp.content)
+url = 'https://police.lehigh.edu/crime-log'
+page= requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#print soup
+'''
+for reportedOn in soup.find_all('span',class_="date-display-single"):
+    wrapDiv = reportedOn.parent.parent.parent
+    if wrapDiv.name == 'div' and wrapDiv['class'] == 'views-field-field-reported-on':
+        print reportedOn'''
+
+for reportedOn in soup.find_all('span',class_='date-display-single'):
+    label = reportedOn.previous_sibling.previous_sibling.getText()
+    if label == 'Reported on:':
+        print reportedOn
+
+for incidentDateTime in soup.find_all('span',class_='date-display-single'):
+    label = incidentDateTime.previous_sibling.previous_sibling.getText()
+    if label == 'Incident Date/Time:':
+        print incidentDateTime
+
+for incidentType in soup.find_all('span',class_='field-content'):
+    if incidentType.parent.name == 'div':
+        print incidentType
