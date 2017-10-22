ff95689e532c9671ed5854c2904f9778b619828b
musa jamshed <Musa@musas-MacBook-Air.local>
2017-10-22 01:47:29 -0400

    Refactored to store each column as a list and then combine them into a dataframe

--- parser/downloader.py
+++ parser/downloader.py
@@ -1,26 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 from downloaderHelper import *
+import pandas as pd
 
 urlTemp = 'https://police.lehigh.edu/crime-log?page='
-
 lastPage = getLastPage()
 
-for eachPage in range(0,lastPage):
-    url = urlTemp + str(lastPage)
+reportedOnList = []
+incidentDateTimeList = []
+dispositionList = []
+incidentTypeList = []
+suspectNameList = []
+incidentLocationList = []
+reportNumberList = []
+descriptionList = []
+allLists = [reportedOnList,incidentDateTimeList,dispositionList,incidentTypeList,suspectNameList,incidentLocationList,reportNumberList,descriptionList]
+
+for eachPage in range(0,lastPage+1):
+    url = urlTemp + str(eachPage)
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     for eachReport in soup.find_all('div', class_='views-row'):
         elements = eachReport.contents
-        print "\n"
-        print getReportedOn(elements)
-        print getIncidentDateTime(elements)
-        print getDisposition(elements)
-        print getIncidentType(elements)
-        print getSuspectName(elements)
-        print getIncidentLocation(elements)
-        print getReportNumber(elements)
-        print getDescription(elements)
+        report = getReport(elements)
+        appendReport(report,allLists)
+
+d = { "Reported_On" : reportedOnList,
+        "Incident_Date_Time" :incidentDateTimeList,
+        "Disposition" : dispositionList,
+        "Incident_Type" : incidentTypeList,
+        "Suspect_Name" : suspectNameList,
+        "Incident_Location" : incidentLocationList,
+        "Report_Number" : reportNumberList,
+        "Description" : descriptionList}
+
+df = pd.DataFrame(d)
+
+print df
+
 
--- parser/downloaderHelper.py
+++ parser/downloaderHelper.py
@@ -9,25 +9,68 @@ def getLastPage(url='https://police.lehigh.edu/crime-log'):
     return int(href[-1:])
 
 def getReportedOn(OGForm):
-    return OGForm[1].contents[1].getText()
+    whole = OGForm[1].contents[1].getText()
+    part = whole[13:]
+    return part
 
 def getIncidentDateTime(OGForm):
-    return OGForm[3].contents[1].getText()
+    whole = OGForm[3].contents[1].getText()
+    part = whole[20:]
+    return part
 
 def getDisposition(OGForm):
-    return OGForm[5].contents[1].getText()
+    whole = OGForm[5].contents[1].getText()
+    part = whole[13:]
+    return part
 
 def getIncidentType(OGForm):
-    return OGForm[7].contents[3].getText()
+    whole = OGForm[7].contents[3].getText()
+    part = whole[9:]
+    return part
 
 def getSuspectName(OGForm):
-    return OGForm[9].contents[1].getText()
+    whole = OGForm[9].contents[1].getText()
+    part = whole[14:]
+    return part
 
 def getIncidentLocation(OGForm):
-    return OGForm[11].contents[1].getText()
+    whole = OGForm[11].contents[1].getText()
+    part = whole[19:]
+    return part
 
 def getReportNumber(OGForm):
-    return OGForm[13].contents[1].getText()
+    whole = OGForm[13].contents[1].getText()
+    part = whole[15:]
+    return part
 
 def getDescription(OGForm):
     return OGForm[15].contents[1].getText()
+
+def getReport(elements):
+    report = []
+    report.append(getReportedOn(elements))
+    report.append(getIncidentDateTime(elements))
+    report.append(getDisposition(elements))
+    report.append(getIncidentType(elements))
+    report.append(getSuspectName(elements))
+    report.append(getIncidentLocation(elements))
+    report.append(getReportNumber(elements))
+    report.append(getDescription(elements))
+    return report
+
+def printReport(elements):
+    print getReportedOn(elements)
+    print getIncidentDateTime(elements)
+    print getDisposition(elements)
+    print getIncidentType(elements)
+    print getSuspectName(elements)
+    print getIncidentLocation(elements)
+    print getReportNumber(elements)
+    print getDescription(elements)
+    print ('\n')
+
+def appendReport(report, allLists):
+    for eachPart in range(0,8):
+        allLists[eachPart].append(report[eachPart])
+    
+    
