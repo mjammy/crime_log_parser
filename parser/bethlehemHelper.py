e054efc451edd68f2b64b0b0d728cccdbdb3e16f
musa jamshed <Musa@musas-Air.home>
2018-01-17 19:28:23 -0500

    Unfortunate had to switch to selenium to get to pages of search results

--- parser/bethlehemHelper.py
+++ parser/bethlehemHelper.py
@@ -8,22 +8,20 @@ from bs4 import BeautifulSoup
 ''' Make POST for searching by date '''
 
 DOCKSHEET_URL = "https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx"
-payload = "ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24ddlSearchType=DateFiled&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24ddlCounty=Northampton&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24ddlCourtOffice=3210&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24drpFiled%24beginDateChildControl%24DateTextBox=01%2F10%2F2018&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24drpFiled%24beginDateChildControl%24DateTextBoxMaskExtender_ClientState=&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24drpFiled%24endDateChildControl%24DateTextBox=01%2F17%2F2018&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24cphSearchControls%24udsDateFiled%24drpFiled%24endDateChildControl%24DateTextBoxMaskExtender_ClientState=&ctl00%24ctl00%24ctl00%24cphMain%24cphDynamicContent%24btnSearch=Search&ctl00%24ctl00%24ctl00%24ctl05%24captchaAnswer=-298923588&__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=32610cda-769b-43c4-8dd7-55b8f14767c3&__VIEWSTATEGENERATOR=4AB257F3&__SCROLLPOSITIONX=0&__SCROLLPOSITIONY=410"
+payload = ""
 headers = {
-    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    'Accept-Encoding': "gzip, deflate, br",
-    'Accept-Language': "en-US,en;q=0.9",
-    'Cache-Control': "no-cache",
-    'Connection': "keep-alive",
-    'Content-Length': "1211",
-    'Content-Type': "application/x-www-form-urlencoded",
-    'Cookie': "f5_cspm=1234; f5avrbbbbbbbbbbbbbbbb=GIIEJFPICDOGFADNHONPCFBIKDAPDKHCMHPKEPHMOBOLIKHEIEMJGCPAENLIKNFKNLMDCMJEEDEODKNOFAHAEPMOILIDIPMFFBPJAGFPHDKPNBGOJGJPJJANGMGAJMCO; ASP.NET_SessionId=5ljcelkwzj0wx3zumsbvbuir; f5avrbbbbbbbbbbbbbbbb=AGCPICJDBILDMLHFHNONKPIDLDONNKNHJAEBPOIFJLJIKLNIFCMNAFHJBEJONDKENFADFDBGIDGNHEDLPANAJOFEILEKBEDBABCPPLEJGNBAIPHKBDBBIILKNEJLIDEB",
-    'Host': "ujsportal.pacourts.us",
-    'Origin': "https://ujsportal.pacourts.us",
-    'Referer': "https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx",
-    'Upgrade-Insecure-Requests': "1",
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-    'Postman-Token': "6f2bc860-5678-df30-0863-c6e9a723a279"
+    'origin': "https://ujsportal.pacourts.us",
+    'upgrade-insecure-requests': "1",
+    'content-type': "application/x-www-form-urlencoded",
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    'x-devtools-emulate-network-conditions-client-id': "(26392B431ACC5F9C636883E3DBF5039F)",
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    'referer': "https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx",
+    'accept-encoding': "gzip, deflate, br",
+    'accept-language': "en-US,en;q=0.9",
+    'cookie': "f5_cspm=1234; ASP.NET_SessionId=5ljcelkwzj0wx3zumsbvbuir; f5avr1149697488aaaaaaaaaaaaaaaa=MPLEAIDIODICPBFIAIPFALHMMGNJHFJELPLHMEIIMMGLKALNNDHOIMANCIOLPPNCEGCCOGJDJJGACOJDKNFAIPFNBGEBBMFOFJGONHCIPADJAOGHAGKGJFLNFOJFNGJA; f5avrbbbbbbbbbbbbbbbb=DDJJNMKIKJGMGLHOFKBBOAPGBOENDGGIILFGDEMNICPIBPCLAEGPJADDCFOJPGDILFGDEIHOAOFCOBJLCDNAKPHLKMMEDNPGMLHPADMIGLAHLCAMEACNBGBDLANKHAKJ; ASP.NET_SessionId=5ljcelkwzj0wx3zumsbvbuir; f5avrbbbbbbbbbbbbbbbb=BKIJHJMAABAGNNLAIOJCGFOJHCFNCHBJGFGIOPGFLFJNBNPFPJBGGPNIOGFELDJEIJEDEEFECAEJLEMCGMJAANFHLNBHALOELJAJIGOEMIBNFACGPMIGMFOLLEAGIKNO",
+    'cache-control': "no-cache",
+    'postman-token': "a88a55d1-41bb-f598-1032-05f296c3929c"
     }
 
 def searchDateRange():
--- parser/bethlehemScraper.py
+++ parser/bethlehemScraper.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 from bethlehemHelper import *
 
-# Searches court records for given dates
-searchResults = searchDateRange()
+''' # Searches court records for given dates
+searchResults = searchstartDate()
 
 for eachRow in range(2,12):
 
@@ -19,4 +19,48 @@ for eachRow in range(2,12):
     
     print("-----ROW " + index + "-----")
     print(case)
-    print(dateOfBirth)
+    print(dateOfBirth) '''
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver.set_page_load_timeout(10)
+
+driver.get('https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx')
+
+# Search type: Date Filed
+selectSearchType = Select(driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$ddlSearchType'))
+selectSearchType.select_by_visible_text('Date Filed')
+
+# County: Northampton
+selectCounty = Select(driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphSearchControls$udsDateFiled$ddlCounty'))
+selectCounty.select_by_visible_text('Northampton')
+
+# Give the website a second to load office choices
+time.sleep(1)
+
+# Court Office: Northampton
+selectCourtOffice = Select(driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphSearchControls$udsDateFiled$ddlCourtOffice'))
+selectCourtOffice.select_by_value("3210")
+
+# Choose Start Date
+startDate = driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphSearchControls$udsDateFiled$drpFiled$beginDateChildControl$DateTextBox')
+for i in range(12): 
+    startDate.send_keys(Keys.LEFT)
+startDate.send_keys("01102018")
+
+# Choose End Date
+startDate = driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphSearchControls$udsDateFiled$drpFiled$endDateChildControl$DateTextBox')
+for i in range(12): 
+    startDate.send_keys(Keys.LEFT)
+startDate.send_keys("01172018")
+
+# Submit Search
+searchButton = driver.find_element_by_name('ctl00$ctl00$ctl00$cphMain$cphDynamicContent$btnSearch')
+searchButton.click()
+
+
+
