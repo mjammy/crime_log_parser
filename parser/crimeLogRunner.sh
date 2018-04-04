d4cc89ff2d473ffee9f44b4d80ac5f53e8e8e0a3
Roshan Giyanani <roshangiyanani@gmail.com>
2018-04-04 17:27:14 -0400

    Updated script to work with server's version of bash (issues with -z check and quotes around arguments).

--- parser/crimeLogRunner.sh
+++ parser/crimeLogRunner.sh
@@ -7,16 +7,13 @@ echo_stderr ()
 }
 
 # Check for git folder location
-if [[ !(-z $1) ]]; then
-    SAVE_LOC=$"$1";
+if [ -n $1 ]; then
+    SAVE_LOC=$1;
     echo "Save location is '$SAVE_LOC'.";
 else
     echo_stderr "ERROR: No save location folder argument given.";
     exit 1;
 fi
 
-echo "Updating python environment."
-pipenv install;
-
 echo "Running downloader.py...";
-pipenv run python downloader.py -d "$SAVE_LOC";
+pipenv run python downloader.py -d $SAVE_LOC;
